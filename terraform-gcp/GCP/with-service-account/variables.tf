f3222f8bdc602f8c61048b36ff39b03c67212fb9
Danail <d_krasimirov@yahoo.com>
2023-01-12 19:25:55 +0100

    connecting from service account

---
+++ terraform-gcp/GCP/with-service-account/variables.tf
@@ -0,0 +1,24 @@
+variable "project_id" {
+
+    type = string
+    description = "The project id"
+  
+}
+
+variable "zone" {
+  
+  type = string
+  description = "The location"
+}
+
+variable "region" {
+  
+  type = string
+  description = "Region"
+ 
+}
+
+variable "bucket_name" {
+type = string
+description = "Bucket name"  
+}
