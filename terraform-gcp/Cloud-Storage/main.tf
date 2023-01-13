dac7db6ac9134c671a99547075ae26d5adc979da
Danail Surudzhiyski <dans@bankingcircle.com>
2023-01-13 10:12:40 +0100

    some fixes to forgotten vars

--- terraform-gcp/Cloud-Storage/main.tf
+++ terraform-gcp/Cloud-Storage/main.tf
@@ -7,4 +7,6 @@ module "cloud-storage" {
   labels = var.labels
   uniform_bucket_level_access = var.uniform_bucket_level_access
   storage_class = var.storage_class
+  bucket_object_name = var.bucket_object_name
+  image_source = var.image_source
 }
