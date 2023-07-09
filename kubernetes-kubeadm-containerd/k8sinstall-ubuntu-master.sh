9b8e116ab79de0e3ce0896d540071f957920134d
dkr290 <39731809+dkr290@users.noreply.github.com>
2023-07-09 22:29:35 +0200

    Update k8sinstall-ubuntu-master.sh

--- kubernetes-kubeadm-containerd/k8sinstall-ubuntu-master.sh
+++ kubernetes-kubeadm-containerd/k8sinstall-ubuntu-master.sh
@@ -98,7 +98,7 @@ version = 2
 EOF
 
 systemctl restart containerd
-curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get install -y kubelet kubeadm kubectl kubernetes-cni
