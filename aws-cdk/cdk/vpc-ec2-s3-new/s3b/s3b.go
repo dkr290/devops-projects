7e5b3c2e67809ed0e9f800e2ca0425c640ef5ddd
Danail <d_krasimirov@yahoo.com>
2024-01-06 17:55:53 +0100

    adding some more cdk

--- aws-cdk/cdk/vpc-ec2-s3-new/s3b/s3b.go
+++ aws-cdk/cdk/vpc-ec2-s3-new/s3b/s3b.go
@@ -2,11 +2,65 @@ package s3b
 
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsiam"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awss3"
 	"github.com/aws/aws-sdk-go-v2/aws"
 )
 
 // Create an S3 bucket
+
+func ReplicationS3Config(stack awscdk.Stack) {
+
+	rbucket := awss3.NewCfnBucket(stack, aws.String("thisistestreoplicabucket49f"), &awss3.CfnBucketProps{
+		BucketName: aws.String("thisistestreoplicabucket49f"),
+		VersioningConfiguration: awss3.CfnBucket_VersioningConfigurationProperty{
+			Status: aws.String("Enabled"),
+		},
+	})
+
+	//IAM role for replication
+	replicationRole := awsiam.NewRole(stack, aws.String("S3ReplicationRole"), &awsiam.RoleProps{
+		AssumedBy: awsiam.NewServicePrincipal(aws.String("s3.amazonaws.com"), nil),
+		RoleName:  aws.String("S3ReplicationRole"),
+		ManagedPolicies: &[]awsiam.IManagedPolicy{
+			awsiam.ManagedPolicy_FromAwsManagedPolicyName(aws.String("AmazonS3FullAccess")), // Attach AmazonS3FullAccess policy
+		},
+	})
+
+	replicationConfig := awss3.CfnBucket_ReplicationConfigurationProperty{
+		Role: replicationRole.RoleArn(),
+		Rules: []awss3.CfnBucket_ReplicationRuleProperty{
+			{
+				Destination: &awss3.CfnBucket_ReplicationDestinationProperty{
+					Bucket: rbucket.BucketName(),
+				},
+				Status:   aws.String("Enabled"),
+				Priority: aws.Float64(1),
+				DeleteMarkerReplication: &awss3.CfnBucket_DeleteMarkerReplicationProperty{
+					Status: aws.String("Enabled"),
+				},
+			},
+		},
+	}
+
+	bucket := awss3.NewCfnBucket(stack, aws.String("thisistestbucket20fgb"), &awss3.CfnBucketProps{
+		BucketName: aws.String("thisistestbucket20fgb"),
+		VersioningConfiguration: awss3.CfnBucket_VersioningConfigurationProperty{
+			Status: aws.String("Enabled"),
+		},
+
+		ReplicationConfiguration: replicationConfig,
+	})
+
+	awscdk.NewCfnOutput(stack, aws.String("MyS3BucketOutput"), &awscdk.CfnOutputProps{
+		Value: bucket.BucketName(),
+	})
+
+	awscdk.NewCfnOutput(stack, aws.String("MyS3ReplicaBucketOutput"), &awscdk.CfnOutputProps{
+
+		Value: rbucket.BucketName(),
+	})
+}
 
 func S3Creation(stack awscdk.Stack) {
 
@@ -28,9 +82,9 @@ func S3Creation(stack awscdk.Stack) {
 		},
 	}
 
-	bucket := awss3.NewBucket(stack, aws.String("thisistestbucket20fgb"), &awss3.BucketProps{
+	bucket_another := awss3.NewBucket(stack, aws.String("thisisanother20fgb"), &awss3.BucketProps{
 
-		BucketName:        aws.String("thisistestbucket20fgb"),
+		BucketName:        aws.String("thisisanotherbucket20fgb"),
 		Versioned:         aws.Bool(true), // Enable versioning
 		PublicReadAccess:  aws.Bool(false),
 		AutoDeleteObjects: aws.Bool(true),
@@ -39,45 +93,9 @@ func S3Creation(stack awscdk.Stack) {
 		LifecycleRules: &lifeCycleRule1,
 	})
 
-	awscdk.NewCfnOutput(stack, aws.String("MyS3BucketOutput"), &awscdk.CfnOutputProps{
-		Value: bucket.BucketName(),
+	awscdk.NewCfnOutput(stack, aws.String("MyS3AnotherBucketOutput"), &awscdk.CfnOutputProps{
+
+		Value: bucket_another.BucketName(),
 	})
-
-	replbucket := awss3.NewBucket(stack, aws.String("thisisreplicabucket20fgb"), &awss3.BucketProps{
-
-		BucketName:        aws.String("thisisreplicabucket20fgb"),
-		Versioned:         aws.Bool(true), // Enable versioning
-		PublicReadAccess:  aws.Bool(false),
-		AutoDeleteObjects: aws.Bool(true),
-		RemovalPolicy:     awscdk.RemovalPolicy_DESTROY,
-
-		LifecycleRules: &lifeCycleRule1,
-	})
-
-	awscdk.NewCfnOutput(stack, aws.String("MyS3ReplicaBucketOutput"), &awscdk.CfnOutputProps{
-
-		Value: replbucket.BucketName(),
-	})
-
-	//IAM role for replication
-	// replicationRole := awsiam.NewRole(stack, aws.String("ReplicationRole"), &awsiam.RoleProps{
-	// 	AssumedBy: awsiam.NewServicePrincipal(aws.String("s3.amazonaws.com"), nil),
-	// })
-
-	// replicationConfig := awss3.CfnBucket_ReplicationConfigurationProperty{
-	// 	Role: replicationRole,
-	// 	Rules: []awss3.CfnBucket_ReplicationRuleProperty{
-	// 		{
-	// 			Destination: &awss3.CfnBucket_ReplicationDestinationProperty{
-	// 				Bucket: replbucket.BucketName(),
-	// 			},
-	// 			Status:   aws.String("Enabled"),
-	// 			Priority: aws.Float64(1),
-	// 			DeleteMarkerReplication: &awss3.CfnBucket_DeleteMarkerReplicationProperty{
-	// 				Status: aws.String("Enabled"),
-	// 			},
-	// 		},
-	// 	},
-	// }
 
 }
