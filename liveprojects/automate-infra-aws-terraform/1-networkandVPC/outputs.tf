18951f03029c4a02f37450734a33305a97f30537
Danail <d_krasimirov@yahoo.com>
2024-07-28 17:59:02 +0200

    adding more outputs

--- liveprojects/automate-infra-aws-terraform/1-networkandVPC/outputs.tf
+++ liveprojects/automate-infra-aws-terraform/1-networkandVPC/outputs.tf
@@ -2,3 +2,36 @@ output "vpc_id" {
   value       = module.network.vpc_id
   description = "The generated vpc id"
 }
+output "route_table_id" {
+  value       = module.network.route_table_id
+  description = "The generated route table"
+}
+
+output "tenancy" {
+  value       = module.network.tenancy
+  description = "The default tenancy"
+}
+
+output "dhcp_options_id" {
+  value       = module.network.dhcp_options_id
+  description = "The default dhcp_options_id"
+}
+
+output "default_security_group_id" {
+  value       = module.network.default_security_group_id
+  description = "The default security group id"
+}
+
+output "publicSubnetA" {
+  value = module.network.publicSubnetA
+}
+
+output "publicSubnetB" {
+  value = module.network.publicSubnetB
+}
+
+output "publicSubnetC" {
+  value = module.network.publicSubnetC
+}
+
+
