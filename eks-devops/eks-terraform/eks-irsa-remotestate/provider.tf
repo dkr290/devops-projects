b18f1d54049db67d6bc6e749511be5e6da0053e7
Danail <d_krasimirov@yahoo.com>
2024-08-31 17:45:34 +0200

    removing helm here

--- eks-devops/eks-terraform/eks-irsa-remotestate/provider.tf
+++ eks-devops/eks-terraform/eks-irsa-remotestate/provider.tf
@@ -28,17 +28,7 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster.token
 
 }
-provider "helm" {
-  kubernetes {
-    host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
-    cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
-      command     = "aws"
-    }
-  }
-}
+
 
 provider "aws" {
   # Configuration options
