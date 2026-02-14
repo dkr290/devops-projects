415af7c6447f27704f6682a863dc09e624586221
dani <d_krasimirov@yahoo>
2026-02-14 22:16:37 +0100

    build now with new sage

--- wan2gp/runpod/wheels/run-sh-future.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker run --rm -t \
-  -e TORCH_CUDA_ARCH_LIST="8.0;8.6;8.9;9.0+PTX" \
-  -v "$PWD:/work" -w /work \
-  runpod/pytorch:2.8.0-py3.11-cuda12.8.1-cudnn-devel-ubuntu22.04 \
-  bash -lc 'python3 -m pip wheel -w /work/wan2gp/runpod/wheels --no-build-isolation flash-attn==2.7.2.post1'
-
